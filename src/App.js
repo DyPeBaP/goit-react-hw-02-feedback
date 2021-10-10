744ca38e8d4593c5640f40a4be1d370cdbfb1900
DyPeBaP <66387850+DyPeBaP@users.noreply.github.com>
2021-10-10 13:57:15 +0300

    new code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import { Component } from "react";
 import FeedbackOptions from "./components/FeedbackOptions/FeedbackOptions";
 import Notification from "./components/Notification/Notification";
 import Section from "./components/Section/Section";
